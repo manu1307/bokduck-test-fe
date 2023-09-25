7e14d80bb70577a5edaf9b6eb3bf73b1d98dcff1
manu1307 <jasonsc@naver.com>
2023-09-25 11:21:01 +0900

    build : netlify 빌드 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { sendUrl } from "./utils/SendUrl";
 
 function App() {
   return (
     <>
       <h1>hello world</h1>
+      <button onClick={() => sendUrl("/login")}>로그인하러 가기</button>
     </>
   );
 }
